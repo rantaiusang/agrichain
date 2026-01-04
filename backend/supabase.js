ac32536cd3dd0f647ca1672aa62c0babe90386f9
rantaiusang <baramultijavapalma@gmail.com>
2026-01-04 09:46:01 +0700

    supabase.js

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,8 +1,6 @@
-// backend/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-// Baca dari environment variables Vercel
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_KEY = process.env.SUPABASE_KEY  // Service Role Key (rahasia)
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
