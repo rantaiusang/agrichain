2a5dfacace6c596520d0e37f2b3ebcc7fb18a3b1
rantaiusang <baramultijavapalma@gmail.com>
2026-01-04 22:02:52 +0700

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // File: api/config.js
 export default async function handler(req, res) {
-  // Baca dari Environment Vercel (Aman)
+  // Mengambil data dari Vercel Environment Variables
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
