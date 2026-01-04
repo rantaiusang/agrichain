743025a4f4e42dee11fa4408837e57aa00b1734a
rantaiusang <baramultijavapalma@gmail.com>
2026-01-04 23:50:43 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// ✅ PERBAIKAN: Menggunakan Anon Key (Safe for Public)
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supa = createClient(supabaseUrl, supabaseKey);
