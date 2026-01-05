aa85642e2a2b098cc9db0cc7a3b86cb7ac8593d6
rantaiusang <baramultijavapalma@gmail.com>
2026-01-05 11:46:49 +0700

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,10 @@
 <script type="module">
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-const SUPABASE_URL = "https://xxxx.supabase.co"
-const SUPABASE_KEY = "sb_publishable_xxxxx"
+const SUPABASE_URL = "https://nkcctncsjmcfsiguowms.supabase.co"
+const SUPABASE_KEY = "sb_publishable_CY2GLPbRJRDcRAyPXzOD4Q_63uR5W9X"
 
 window.supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+console.log("Supabase READY", window.supabase)
 </script>
