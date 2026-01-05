e7deffecfb7881f67ea47edaab063481ccb95edb
rantaiusang <baramultijavapalma@gmail.com>
2026-01-05 15:21:57 +0700

    Delete register.js

--- register.js
+++
@@ -1,7 +0,0 @@
-import { register } from '../backend/auth.js';
-
-export default async function handler(req, res) {
-    const { email, password, fullName, role } = req.body;
-    const result = await register(email, password, fullName, role);
-    return res.status(200).json(result);
-}
